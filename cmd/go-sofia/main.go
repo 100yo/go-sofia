c65613082f39be42a847fc3d4de575f6ab7bdf68
100yo <stoyan.vellev@sap.com>
2018-10-06 15:11:48 +0300

    Add improvements and minor adjustments

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -3,6 +3,7 @@ package main
 import ("log"
 "os"
 "fmt"
+"time"
  "net/http"
  "github.com/gorilla/mux"
  "github.com/100yo/go-sofia/internal/diagnostics"
@@ -21,12 +22,12 @@ func main() {
 	blPort := os.Getenv("PORT")
 	if len(blPort) == 0 {
 		log.Fatal("The application port should be set")
-	}	
+	}
 
 	diagPort := os.Getenv("DIAG_PORT")
 	if len(diagPort) == 0 {
 		log.Fatal("The diagnostics port should be set")
-	}	
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
@@ -50,14 +51,14 @@ func main() {
 
 	for _, c := range configurations {
 		go func (conf serverConf) {
-			server := &http.Server{
+			server := &http.Server {
 				Addr: ":" + conf.port,
 				handler: conf.router,
 			}
 			err := server.ListenAndServe()
 			// server.Shutdown()
 			if err != nil {
-				possibleErrors <- err	
+				possibleErrors <- err
 			}
 			
 		}(c)
@@ -73,14 +74,30 @@ func main() {
 			}
 		err := server.ListenAndServe()
 		if err != nil {
-			possibleErrors <- err	
+			possibleErrors <- err
 		}
 	}()
 
 	select {
 	case err := <-possibleErrors:
-		log.Fatal(err)
+		log.Printf("Received error: %v", err)
+	case sig := <-interrupt:
+		log.Printf("Received the signal: %v", sig)
 	}
+
+	for _, s := range servers {
+		timeout := 5 * time.Second
+		log.Printf("Continue with timeout: %s\n", timeout)
+		ctx, cancel := context.WithTimeout(context.Background, timeout)
+		defer cancel()
+		customError := s.Shutdown(ctx)
+		if customError != nil {
+			fmt.Println(customError)
+		}
+		log.Printf("Server gravecully stopped")
+	}
+	log.Fatal(err)
+
 }
  func hello(w http.ResponseWriter, r *http.Request) {
  	log.Print("The hello handler was called")
